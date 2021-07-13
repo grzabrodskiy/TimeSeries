c312c918ac8a697bfeec68499328de3a421d9f71
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2021-07-13 12:30:48 +0200

    Update i_exnaton_repository.dart
    Added Comments

--- exnaton_frontend/lib/model/repository/i_exnaton_repository.dart
+++ exnaton_frontend/lib/model/repository/i_exnaton_repository.dart
@@ -1,3 +1,7 @@
+
+// interface for ExnatonRepository
+// not really needed, just enforces havinf getAllMeasurements() method that returns the list of measurements
+
 import 'package:exnaton_frontend/model/measurement.dart';
 
 abstract class IExnatonRepository {
