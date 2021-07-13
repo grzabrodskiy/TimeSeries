3d8f50aba4e4e432c1bd37c96819e1f06f449949
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2021-07-13 13:08:11 +0200

    Update time_series_screen.dart
    Added Comments

--- exnaton_frontend/lib/ui/time_series_screen.dart
+++ exnaton_frontend/lib/ui/time_series_screen.dart
@@ -8,27 +8,32 @@ import 'package:flutter/material.dart';
 import 'package:exnaton_frontend/utility/date_time_extension.dart';
 import 'package:intl/intl.dart';
 
+// TimeSeriesScreen is StatefulWidget
+// states are defined by different values of radio buttons and a value of a checkbox
 class TimeSeriesScreen extends StatefulWidget {
+  // constructor
   TimeSeriesScreen({Key? key}) : super(key: key);
-
+  // state creation method
   @override
   _TimeSeriesScreenState createState() => _TimeSeriesScreenState();
 }
 
 class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
+  // using interface rather than class (not important)
   late final IExnatonRepository repository;
-
-  DateTime? chosenDate;
-  TimePeriod? timePeriod = TimePeriod.month;
-  bool grouped = false;
-
+  
+  DateTime? chosenDate; // data that is chosen on the righ
+  TimePeriod? timePeriod = TimePeriod.month; // radiobutton value (default to month)
+  bool grouped = false; // checkbox value (default to unchecked)
+  
+  // read from backend API the list of measurements into repository variable
   @override
   void initState() {
     super.initState();
     repository =
         ExnatonRepository('${Uri.base.scheme}://${Uri.base.host}', port: 5000);
   }
-
+  // describes widget UI (state-based)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,12 +46,12 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
             (BuildContext context, AsyncSnapshot<List<Measurement>> snapshot) {
           if (snapshot.hasData) {
             final filteredData = !this.grouped
-                ? filteredMeasurements(
+                ? filteredMeasurements( // ungrouped data - only take data for a specific time period
                     snapshot.data!,
                     this.timePeriod!,
                     this.chosenDate ??
                         DateTime.parse(snapshot.data!.last.timestamp))
-                : snapshot.data!;
+                : snapshot.data!; // grouped data - take all the data 
             return Row(
               children: [
                 Expanded(
@@ -54,6 +59,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      // show graph based on periods
                       _buildPeriodsWidget(context),
                       Row(
                         mainAxisSize: MainAxisSize.min,
@@ -62,14 +68,15 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
                         ],
                       ),
                       Container(
+                        // sizing the graph based on screen size
                         height:
                             (MediaQuery.of(context).size.width * 0.7) * 9 / 16,
                         child: charts.TimeSeriesChart(
                           [
                             getSeries(
                                 filteredData,
-                                this.timePeriod ?? TimePeriod.month,
-                                this.chosenDate ??
+                                this.timePeriod ?? TimePeriod.month, // take time period (month if not specified)
+                                this.chosenDate ?? // take selected date (first date if not selected)
                                     DateTime.parse(
                                         snapshot.data![0].timestamp)),
                           ],
@@ -98,7 +105,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
       ),
     );
   }
-
+  // periods graph
   Widget _buildPeriodsWidget(BuildContext context) {
     return Card(
       child: Padding(
@@ -112,6 +119,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
                   Radio<TimePeriod>(
                     value: period,
                     groupValue: this.timePeriod,
+                    // change state when different option is selected 
                     onChanged: (newPeriod) => setState(() {
                       this.timePeriod = newPeriod;
                     }),
@@ -124,6 +132,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
               children: [
                 Checkbox(
                   value: this.grouped,
+                   // change state when different option is selected 
                   onChanged: (newValue) => setState(() {
                     this.grouped = newValue!;
                     this.chosenDate = null;
@@ -137,7 +146,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
       ),
     );
   }
-
+  // list of measurements (date and value)
   Widget _buildMeasurementsList(BuildContext context, List<Measurement> data) {
     return ListView(
       children: [
@@ -167,6 +176,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
         ),
         ...data.map((m) {
           return InkWell(
+            // change state if new date is selected 
             onTap: () => setState(() {
               this.chosenDate = DateTime.parse(m.timestamp);
               this.grouped = false;
@@ -209,19 +219,19 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
       ],
     );
   }
-
+  // progress indicator (never shown - we are moving too fast :)
   Widget _buildLoadingIndicator(BuildContext context) {
     return Center(
       child: CircularProgressIndicator(),
     );
   }
-
+  // error indicator (hopefully never seen :)
   Widget _buildErrorWidget(BuildContext context, Object? error) {
     return Center(
       child: Text("Unknown error"),
     );
   }
-
+  // setting up the chart series
   charts.Series<dynamic, DateTime> getSeries(
     List<Measurement> data,
     TimePeriod period,
@@ -252,7 +262,7 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
           measurements.length,
     );
   }
-
+  // this method filters measurements based on time perod (day/week/month) and date selected (dateTime)
   List<Measurement> filteredMeasurements(
       List<Measurement> data, TimePeriod period, DateTime dateTime) {
     switch (period) {
@@ -281,9 +291,10 @@ class _TimeSeriesScreenState extends State<TimeSeriesScreen> {
     }
   }
 }
-
+// time period allowed values
 enum TimePeriod { hour, day, week, month }
 
+// naming of the enum values
 extension TimePeriodExtension on TimePeriod {
   String get title {
     switch (this) {
