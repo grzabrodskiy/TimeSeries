99232ee644a3387055266448a547072ca09f84af
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2021-07-13 12:23:03 +0200

    Update main.dart
    Added Comments

--- exnaton_frontend/lib/main.dart
+++ exnaton_frontend/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:exnaton_frontend/ui/time_series_screen.dart';
 import 'package:flutter/material.dart';
-
+// front end main function running MyApp()
 void main() {
   runApp(MyApp());
 }
-
+// Calling TimeSeriesScreen() with some additional settings
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
