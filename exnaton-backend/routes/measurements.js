fcb7ec2a58e14272e189bc02e9620471aa102ef0
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2021-07-13 12:11:28 +0200

    Update measurements.js
    Added comments

--- exnaton-backend/routes/measurements.js
+++ exnaton-backend/routes/measurements.js
@@ -1,17 +1,22 @@
 'use strict';
+// create router out of Express.js
 const Router = require('express');
 
+// API to retrieve measurements from the server 
+// the measurements are stored in MongoDB (collection 'measurements')
 const getMeasurements = (app, db) => {
     const router = new Router();
 
+    // HTTP GET method - return all measurements
     router.get('/', async (req, res) => {
         const measurements = await db.collection('measurements').find({}, {_id: 0, "date": 0}).toArray();
         res.status(200).json({measurements: measurements}).end();
     });
-
+    // HTTP GET method to /group - return grouped measurements: supports grouppedBy={month|day|weekday}
     router.get('/grouped', async (req, res) => {
         const groupedBy = req.query.by;
         if (groupedBy == "month") {
+            // group by query to MongoDB aggreging by month
             const measurementsGroupedByMonth = await db.collection('measurements').aggregate([
                 {
                     $group : {
@@ -48,5 +53,5 @@ const getMeasurements = (app, db) => {
 
     app.use('/measurements', router);
 }
-
+// defaul method of the module to be exported
 module.exports = getMeasurements;
