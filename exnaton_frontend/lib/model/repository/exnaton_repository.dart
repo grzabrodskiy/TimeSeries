771cec82c3f9cce62fc6e91a762f231cf9a28af7
Gregory Zabrodskiy <grzabrodskiy@gmail.com>
2021-07-14 19:23:46 +0200

    Update exnaton_repository.dart
    added comments

--- exnaton_frontend/lib/model/repository/exnaton_repository.dart
+++ exnaton_frontend/lib/model/repository/exnaton_repository.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:exnaton_frontend/model/measurement.dart';
 import 'package:exnaton_frontend/model/repository/i_exnaton_repository.dart';
 
-class ExnatonRepository extends IExnatonRepository {
+class ExnatonRepository extends IExnatonRepository {// create repository, obstruction data access layer
   final String baseUrl;
   final int port;
 
@@ -14,7 +14,7 @@ class ExnatonRepository extends IExnatonRepository {
   // overriding from the interface
   // reading the measurements into the list of Measurement structures
   @override
-  Future<List<Measurement>> getAllMeasurements() async {
+  Future<List<Measurement>> getAllMeasurements() async {// basically takes all backend stuff and moves to List called Measurement
     var response = await http.get(
       Uri.parse('$baseUrl:$port/measurements'),
     );
@@ -23,18 +23,27 @@ class ExnatonRepository extends IExnatonRepository {
       // read from JSON to Measurement structure
       var languages =
           jsonDecode(response.body)['measurements'] as List<dynamic>;
-      return languages.map((e) {
-        return Measurement(
+      return languages.map((e) { // maps JSON to the structure that we wanted
+        return Measurement(// Constructor of Measurement, Capital Measurement is a structure, convert JSON to it
             measurement: e['measurement'],
             positiveEnergy: e['positiveEnergy'],
             negativeEnergy: e['negativeEnergy'],
             balanceEnergy: e['balanceEnergy'],
-            tags: e['tags'],
-            timestamp: e['timestamp']);
-      }).toList();
+            tags: e['tags'],// e for element in the JSON, lower case "measurement" is attribute of structure
+            timestamp: e['timestamp']); // we've converted list of JSON strings to list of Measurement Objects
+      }).toList(); // returns Future of <List> of Measurements
     } else {
       throw HttpException(response.reasonPhrase ?? 'Unknown error',
           uri: response.request?.url);
     }
   }
 }
+
+
+// balanced=positive, negative=0
+// we only use timestamp and balanced(which is technically positive)
+// balanced = positive - negative
+
+// thought of doing stack chart but saw that always 0, bar chart
+
+// show 7 day running average
